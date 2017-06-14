97a640d2a53881adba604c234044597e585e7e7d
Dan Andrei <andreidani96@gmail.com>
2017-06-14 18:25:07 +0300

    use vis.js to render buttons

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// Dependencies
-const Button = require('./lib/button');
-
 module.exports = (network, config) => {
     let self = {};
     self.network = network;
@@ -15,148 +12,271 @@ module.exports = (network, config) => {
     self.contexts = config.contexts;
     self.menu = null;
 
-    // get canvas context
-    self.network.on('afterDrawing', (ctx) => {
+    // export methods
+    self.toggle = toggle;
+    self.show = show;
+    self.hide = hide;
 
-        if (self.menu) {
-            if (self.menu.context.type === 'point') {
-                ctx.beginPath();
-                ctx.arc(self.menu.center.x, self.menu.center.y, 2, 0 , 2 * Math.PI);
-                ctx.fillStyle = '#000000';
-                ctx.fill();
-            }
+    // change cursor when hovering buttons
+    self.network.on('hoverNode', e => {
+        let node = self.network.body.data.nodes.get(e.node);
 
-            self.menu.buttons.forEach(btn => {
-                btn.draw(ctx);
-            });
+        if (node.type === 'button') {
+            self.network.canvas.body.container.style.cursor = 'pointer';
+            self.network.body.data.nodes.update({id: e.node, font: { bold: true }});
         }
-        ctx.save();
     });
 
-    // button mouseover and mouseaway selfs
-    let canvas = self.network.canvas.frame.canvas;
-    let canvasOffset = canvas.getBoundingClientRect();
-    canvas.addEventListener('mousemove', event => {
+    self.network.on('blurNode', e => {
+        let node = self.network.body.data.nodes.get(e.node);
 
-        if (!self.menu) {
+        if (node.type === 'button') {
+            self.network.canvas.body.container.style.cursor = 'default';
+            self.network.body.data.nodes.update({id: e.node, font: { bold: false }});
+        }
+    });
+
+    // listen for click events
+    self.network.on('click', e => {
+        if (!e.nodes || !e.nodes.length) {
             return;
         }
 
-        let mouse = self.network.DOMtoCanvas({
-            x: event.offsetX - canvasOffset.left,
-            y: event.offsetY - canvasOffset.top
-        });
-
-        self.menu.buttons.forEach(button => {
-            let distance = Math.sqrt((mouse.x - button.x)*(mouse.x - button.x) + (mouse.y - button.y)*(mouse.y - button.y));
-            if (distance < button.radius) {
-                if (!button.isHover) {
-                    button.hover();
-                }
-            } else {
-                if (button.isHover) {
-                    button.blur();
-                }
-            }
-        });
-    }, false);
-
-    // listen for click events on the buttons
-    self.network.on('click', event => {
-
-        if (!self.menu) {
+        let node = self.network.body.data.nodes.get(e.nodes[0]);
+        if (!node.type || node.type !== 'button' || !node.event) {
             return;
         }
 
-        let mouse = {
+        console.log(node.event);
+
+        self.network.unselectAll();
+    });
+
+    return self;
+}
+
+function toggle (data, args) {
+    args = args || {};
+
+    if (!args.context || !this.contexts[args.context] || !data.event) {
+        return;
+    }
+
+    let context = this.contexts[args.context];
+    if (!context.buttons || !context.buttons.length) {
+        return;
+    }
+
+    if (this.menu) {
+        // if a menu already exists close it or create another one
+        if (context.type !== this.menu.context.type || (context.type === 'node' && this.menu.nodeId !== data.event.nodes[0])) {
+            this.hide(data.event, context);
+            this.show(data.event, context);
+        } else {
+            this.hide(data.event, context);
+        }
+    } else {
+        this.show(data.event, context);
+    }
+}
+
+// show context menu
+function show (event, context) {
+
+    if (this.menu) {
+        return;
+    }
+
+    // init menu
+    let center;
+    if (context.type === 'point') {
+        center = {
             x: event.pointer.canvas.x,
             y: event.pointer.canvas.y
         };
+    } else if (context.type === 'node') {
+        let pos = this.network.getPositions(event.nodes[0]);
+        center = pos[event.nodes[0]];
+    }
 
-        self.menu.buttons.forEach(button => {
-            let distance = Math.sqrt((mouse.x - button.x)*(mouse.x - button.x) + (mouse.y - button.y)*(mouse.y - button.y));
-            if (distance < button.radius) {
-                console.log('click', button);
-            }
-        });
-    });
-
-    // export methods
-    self.toggle = (data, args) => {
-        args = args || {};
-
-        if (!args.context || !self.contexts[args.context] || !data.event) {
-            return;
-        }
-
-        let context = self.contexts[args.context];
-        if (!context.buttons || !context.buttons.length) {
-            return;
-        }
-
-        // for point context only
-        if (context.type === 'point' && self.menu && self.menu.context.type === 'point') {
-            self.menu = null;
-            self.network.redraw();
-            return;
-        }
-
-        // TODO CONFIGURE
-        // const circle constants
-        const minRadius = 90;
-        const buttonRadius = 20;
-        const buttonDistance = 15;
-
-        let center;
-        if (context.type === 'point') {
-            center = {
-                x: data.event.pointer.canvas.x,
-                y: data.event.pointer.canvas.y
-            };
-        } else if (context.type === 'node') {
-            let pos = self.network.getPositions(data.event.nodes[0]);
-            center = pos[data.event.nodes[0]];
-        }
-
-        // init menu
-        let menu = {
-            context: context,
-            center: center,
-            buttons: []
-        };
-
-        /* COMPUTE BUTTON POSITIONS */
-
-        // step 1. Compute the radius of the button circle
-        let potentialCircumference = context.buttons.length * (2 * buttonRadius + buttonDistance);
-        let potentialRadius = potentialCircumference / (Math.PI * 2);
-
-        let radius = (minRadius > potentialRadius) ? minRadius : potentialRadius;
-
-        // step 2. Compute the angle that contains a single button
-        let a = buttonRadius + buttonDistance / 2;
-        let b = radius;
-        let c = radius;
-        let cosA = (b*b + c*c - a*a) / (2*b*c);
-        let angle = Math.acos(cosA) * 2;
-
-        // step 3. compute positions of buttons
-        let cAngle = -1.5;
-        context.buttons.forEach(btn => {
-            let x = radius * Math.cos(cAngle) + menu.center.x;
-            let y = radius * Math.sin(cAngle) + menu.center.y;
-
-            menu.buttons.push(new Button(x, y, buttonRadius, {
-                color: (btn.color && self.config.colors[btn.color]) ? self.config.colors[btn.color] : {},
-                icon: btn.icon
-            }));
-
-            cAngle += angle;
-        });
-
-        self.menu = menu;
-        self.network.redraw();
+    // create the menu
+    let menu = {
+        context: context,
+        center: center,
+        buttons: createButtons.call(this, center, {
+            buttons: context.buttons,
+            minRadius: 100,
+            buttonRadius: 15,
+            buttonDistance: 45
+        })
     };
 
-    return self;
+    // freeze the node if it is the center
+    if (context.type === 'node') {
+        this.network.body.data.nodes.update({id: event.nodes[0], fixed: true});
+        menu.nodeId = event.nodes[0]
+
+        // create some edges
+        menu.edges = [];
+        menu.buttons.forEach(btn => {
+
+            let edge = {
+                id: 'edge_' + generateButtonId(),
+                from: event.nodes[0],
+                to: btn.id,
+                fixed: true,
+                physics: false,
+                arrows: {
+                    to: false
+                },
+                width: 0.1,
+                hoverWidth: 0.1,
+                selectionWidth: 0.1,
+                color: {
+                    color: '#D4DFF4',
+                    highlight: '#D4DFF4',
+                    hover: '#D4DFF4'
+                }
+            };
+            menu.edges.push(edge);
+        });
+    } else {
+        // add a node at the center
+        menu.centerNode = {
+            id: 'center_' + generateButtonId(),
+            fixed: true,
+            physics: false,
+            size: 1,
+            x: center.x,
+            y: center.y
+        };
+
+        // create some edges
+        menu.edges = [];
+        menu.buttons.forEach(btn => {
+
+            let edge = {
+                id: 'edge_' + generateButtonId(),
+                from: menu.centerNode.id,
+                to: btn.id,
+                fixed: true,
+                physics: false,
+                arrows: {
+                    to: false
+                },
+                width: 0.1,
+                hoverWidth: 0.1,
+                selectionWidth: 0.1,
+                color: {
+                    color: '#D4DFF4',
+                    highlight: '#D4DFF4',
+                    hover: '#D4DFF4'
+                }
+            };
+            menu.edges.push(edge);
+        });
+
+    }
+
+    // add buttons to canvas
+    this.menu = menu;
+    this.network.body.data.nodes.add(menu.buttons);
+    if (menu.centerNode) {
+        this.network.body.data.nodes.add([menu.centerNode]);
+    }
+    if (menu.edges) {
+        this.network.body.data.edges.add(menu.edges);
+    }
+
+}
+
+// hide context menu
+function hide (event, context) {
+
+    if (!this.menu) {
+        return;
+    }
+
+    // remove the buttons from the graph
+    this.network.body.data.nodes.remove(this.menu.buttons);
+
+    if (this.menu.edges) {
+        this.network.body.data.edges.remove(this.menu.edges);
+    }
+
+    if (this.menu.centerNode) {
+        this.network.body.data.nodes.remove([this.menu.centerNode]);
+    }
+
+    // unfreeze the center node
+    if (this.menu.context.type === 'node') {
+        this.network.body.data.nodes.update({id: this.menu.nodeId, fixed: false});
+    }
+
+    this.menu = null;
+}
+
+/* Private functions */
+
+function createButtons (center, config) {
+    config = config || {};
+    config.buttons = config.buttons || 0;
+    config.minRadius = config.minRadius || 90;
+    config.buttonRadius = config.buttonRadius || 20;
+    config.buttonDistance = config.buttonDistance || 30;
+
+    // step 1. Compute the radius of the button circle
+    let potentialCircumference = config.buttons.length * (2 * config.buttonRadius + config.buttonDistance);
+    let potentialRadius = potentialCircumference / (Math.PI * 2);
+
+    let radius = (config.minRadius > potentialRadius) ? config.minRadius : potentialRadius;
+
+    // step 2. Compute the angle that contains a single button
+    let a = config.buttonRadius + config.buttonDistance / 2;
+    let b = radius;
+    let c = radius;
+    let cosA = (b * b + c * c - a * a) / (2 * b * c);
+    let angle = Math.acos(cosA) * 2;
+
+    // step 3. compute positions of buttons
+    let cAngle = -1.5;
+    let buttons = [];
+    config.buttons.forEach(btnConfig => {
+
+        let button = {
+            id: 'button_' + generateButtonId(),
+            type: 'button',
+            x: radius * Math.cos(cAngle) + center.x,
+            y: radius * Math.sin(cAngle) + center.y,
+            physics: false,
+            fixed: true,
+            size: config.buttonRadius,
+            label: btnConfig.label,
+            font: btnConfig.font,
+            event: btnConfig.event
+        };
+
+        // add colors
+        if (typeof btnConfig.color === 'string') {
+            button.color = this.config.colors[btnConfig.color];
+        }
+
+        // add icons
+
+        buttons.push(button);
+        cAngle += angle;
+    });
+
+    return buttons;
+}
+
+function generateButtonId () {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < 10; i++ ) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
 }
